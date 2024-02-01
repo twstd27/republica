6f1f4566d0e4c921f081c0e49f5a541aa079c941
Rodrigo Cruz <rodrigocruzh@gmail.com>
2024-02-01 00:29:37 -0400

    netifly

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
   integrations: [tailwind(), icon()],
+  output: "server",
+  adapter: netlify()
 });
